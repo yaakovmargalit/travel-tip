5aeb045226debb2b9c10387b74af30abab9a0155
amit <amitush27@gmail.com>
2021-11-03 14:38:48 +0200

    test

--- js/services/map.service.js
+++ js/services/map.service.js
@@ -40,7 +40,7 @@ function panTo(lat, lng) {
 
 function _connectGoogleApi() {
     if (window.google) return Promise.resolve()
-    const API_KEY = ''; //TODO: Enter your API Key
+    const API_KEY = 'AIzaSyDbCgiLMOvSqpuGSWeahVxane6dGFfR_CA' 
     var elGoogleApi = document.createElement('script');
     elGoogleApi.src = `https://maps.googleapis.com/maps/api/js?key=${API_KEY}`;
     elGoogleApi.async = true;
